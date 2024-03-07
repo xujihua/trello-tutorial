b77802988be9b89c849e08d2c4a89a6c1e5858ae
xujih <2457763915@qq.com>
2024-03-07 22:01:38 +0800

    提交

---
+++ app/(platform)/(clerk)/layout.tsx
@@ -0,0 +1,9 @@
+const ClerkLayout=({children}:{children:React.ReactNode})=>{
+    return (
+        <div className="h-full flex intems-center justify-center">
+            {children}
+        </div>
+    )
+};
+
+export default ClerkLayout;
